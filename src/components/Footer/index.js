43657c34fb9893f30ca10637f869eddebcd7feb0
axeliono <69375446+axeliono@users.noreply.github.com>
2021-03-16 18:55:13 -0500

    added 3rd social media

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,7 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import GithubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import FacebookIcon from '@material-ui/icons/Facebook'
 
 const Footer = () => {
   return (
@@ -18,6 +19,12 @@ const Footer = () => {
           label="LinkedIn"
           icon={<LinkedInIcon />}
           href="https://www.linkedin.com/in/chandleregreen/"
+          target="_blank"
+        />
+        <BottomNavigationAction
+          label="Facebook"
+          icon={<FacebookIcon />}
+          href="https://www.facebook.com/Axeliono"
           target="_blank"
         />
       </BottomNavigation>
